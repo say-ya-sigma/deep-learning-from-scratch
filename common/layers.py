fix softmax with loss layerの型をつけた

common/layers.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import numpy as np
+from numpy.typing import NDArray
 from common.functions import *
 from common.util import im2col, col2im
 
@@ -73,7 +74,7 @@ class SoftmaxWithLoss:
         self.y = None # softmaxの出力
         self.t = None # 教師データ
 
-    def forward(self, x, t):
+    def forward(self, x: NDArray[np.floating], t: NDArray[np.floating]):
         self.t = t
         self.y = softmax(x)
         self.loss = cross_entropy_error(self.y, self.t)
@@ -83,7 +84,7 @@ class SoftmaxWithLoss:
     def backward(self, dout=1):
         batch_size = self.t.shape[0]
         if self.t.size == self.y.size: # 教師データがone-hot-vectorの場合
-            dx = (self.y - self.t) / batch_size
+            dx: NDArray[np.floating] = (self.y - self.t) / batch_size
         else:
             dx = self.y.copy()
             dx[np.arange(batch_size), self.t] -= 1